151274ebbaab65ad678d3906682202165a6e3fc5
yiouejv <yiouejv@126.com>
2018-11-10 17:44:49 +0800

    add CSRF_Protect

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from apps.icbc.views import bp as icbc_bp
 from apps.viruses.views import bp as viruses_bp
+from flask_wtf import CSRFProtect
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 app.register_blueprint(icbc_bp)
 app.register_blueprint(viruses_bp)
 
+CSRFProtect(app)
 
 if __name__ == '__main__':
     app.run()
